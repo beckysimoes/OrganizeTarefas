0d879a04c3a8b9d80502532233b98592ee48dd2f
Rebeca Simões <rebeca.simoes@discente.uenp.edu.br>
2024-12-06 20:26:10 -0300

    Adicionar a função de tarefas no arquivo tarefas.js

---
+++ tarefas.js
@@ -0,0 +1,11 @@
+// Contador de Tarefas
+
+let tarefas = [];
+
+function adicionarTarefa(tarefa) {
+    tarefas.push(tarefas);
+    console.log('Tarefa "$ {tarefa}" adicionada.');
+}
+
+adicionarTarefa("Estudar Git");
+adicionarTarefa("Revisar código do projeto");
