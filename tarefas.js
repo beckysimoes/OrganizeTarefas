3c8cc15598ed1312d318535d8d749e2529887d3e
Rebeca Simões <rebeca.simoes@discente.uenp.edu.br>
2024-12-06 21:23:10 -0300

    Resolvendo conflitos de marge

--- tarefas.js
+++ tarefas.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-// Contador de Tarefas
-
-let tarefas = [];
-
-function adicionarTarefa(tarefa) {
-    tarefas.push(tarefas);
-    console.log('Tarefa "$ {tarefa}" adicionada.');
+function adicionarTarefa() 
+{
+    console.log("Adicionando tarefa da branch2");
 }
-
-adicionarTarefa("Estudar Git");
-adicionarTarefa("Revisar código do projeto");
-=======
-
->>>>>>> 981879f93b215f097b4d619d220d223011e02efa
