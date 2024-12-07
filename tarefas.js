bb224f520f832569440ebdfb67bfcaad38812096
beckysimoes <rebeca.simoes@discente.uenp.edu.br>
2024-12-06 23:09:31 -0300

    Update tarefas.js

--- tarefas.js
+++ tarefas.js
@@ -1,1 +1,17 @@
+// Função para adicionar uma tarefa
+function adicionarTarefa(tarefa) {
+    tarefas.push(tarefa);
+    console.log('Tarefa "${tarefa}" adicionada com sucesso!');
+}
 
+// Função para listar as tarefas
+function listarTarefas() {
+    if (tarefas.length === 0) {
+        console.log("Não há tarefas para mostrar.");
+    } else {
+        console.log("Lista de Tarefas:");
+        tarefas.forEach((tarefa, index) => {
+            console.log('${index + 1}. ${tarefa}');
+        });
+    }
+}
